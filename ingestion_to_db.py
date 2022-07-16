966040fcf914620b34672ca846b13004b2df625b
alanucamargo <31542924+alanucamargo@users.noreply.github.com>
2022-07-16 13:22:00 -0500

    Create ingestion_to_db.py

---
+++ ingestion_to_db.py
@@ -0,0 +1,12 @@
+from airflow.models import DAG
+from airflow.operators.dummy import DummyOperator
+
+with DAG('db_ingestion') as dag:
+    start_workflow = DummyOperator(task_id='start_workflow')
+    validate = DummyOperator(task_id='validate')
+    prepare = DummyOperator(task_id='prepare')
+    load = DummyOperator(task_id='load')
+    end_workflow = DummyOperator(task_id='end_workflow')
+
+    #We setup here the order of the tasks
+    start_workflow >> validate >> prepare >> load >> end_workflow
