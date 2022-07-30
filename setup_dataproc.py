6a295794e471fd032a06a6942020376b5af7758d
alanucamargo <31542924+alanucamargo@users.noreply.github.com>
2022-07-30 11:07:23 -0500

    Update setup_dataproc.py

--- setup_dataproc.py
+++ setup_dataproc.py
@@ -16,20 +16,45 @@ from airflow.providers.google.cloud.hooks.gcs import GCSHook
 #Consideramos las librerias para trabajar con dataproc
 from airflow.providers.google.cloud.operators.dataproc import DataprocCreateClusterOperator, DataprocDeleteClusterOperator
 
+REGION = 'us-central1-a'
+CLUSTER_NAME = 'dataproc_alan'
+
+CLUSTER_CONFIG = {
+  "configBucket": "us-central1-de-bootcamp-786ac1aa-bucket",
+  "tempBucket": "us-central1-de-bootcamp-786ac1aa-bucket",
+  "gceClusterConfig": {
+    object (GceClusterConfig)
+  },
+  "masterConfig": {
+      "numInstances": 1,
+      "machineTypeUri": "n1-standard-1",
+      "disk_config": {"boot_disk_type": "pd-standard", "boot_disk_size_gb": 1024}
+  },
+  "workerConfig": {
+      "numInstances": 2,
+      "machineTypeUri": "n1-standard-1",
+      "disk_config": {"boot_disk_type": "pd-standard", "boot_disk_size_gb": 1024}
+  },
+}
 
 with DAG(
     'setup_postgres', start_date=days_ago(1), schedule_interval='@once'
     ) as dag:
     dag.doc_md = __doc__
     start_workflow = DummyOperator(task_id='start_workflow')
-    create_cluster = DummyOperator(task_id='create_cluster')
+    create_cluster = DataprocCreateClusterOperator(task_id='create_cluster',
+                    #project_id = PROJECT_ID,
+                    cluster_config = CLUSTER_CONFIG,
+                    region = REGION,
+                    cluster_name = CLUSTER_NAME,
+                    gcp_conn_id='google_cloud_default')
     validate = DummyOperator(task_id='validate')
     prepare = DummyOperator(task_id='prepare')
-    clear = DummyOperator(task_id='clear')
-    continue_workflow = DummyOperator(task_id = 'continue_workflow')
-    branch = DummyOperator(task_id = 'is_empty')
     load = DummyOperator(task_id='load')
-    delete_cluster = DummyOperator(task_id='delete_cluster')
+    delete_cluster = DataprocDeleteClusterOperator(task_id='delete_cluster',
+                    region = REGION,
+                    cluster_name = CLUSTER_NAME,
+                    gcp_conn_id='google_cloud_default')
     end_workflow = DummyOperator(task_id='end_workflow')
 
     #We setup here the order of the tasks
